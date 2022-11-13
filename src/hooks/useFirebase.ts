8a362492b0521b70c85a73f092926878f6bca3ca
Sato Taisei <t.sei43rm@gmail.com>
2022-11-13 18:05:15 +0900

    ✏️ fix to uppercase

--- src/hooks/useFirebase.ts
+++ src/hooks/useFirebase.ts
@@ -2,7 +2,7 @@ import { getDatabase, ref, onValue, push } from 'firebase/database';
 import { useEffect, useState } from 'react';
 
 import { firebaseApp } from '@/services/firebase';
-import { Todo, TodoList } from '@/types/todo';
+import { Todo, TodoList } from '@/types/Todo';
 
 export const useFirebase = (path: string) => {
     const [todoList, setTodoList] = useState<TodoList>();
