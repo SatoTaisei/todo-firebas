75cd37397aa16dc54a48efe034bde8f8638b1c18
Sato Taisei <t.sei43rm@gmail.com>
2022-11-17 08:22:56 +0900

    ✨ add Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export const Header = () => {
     return (
-        <header>
-            <span>Todo</span>
+        <header className="absolute top-0 bg-gray-900 text-2xl text-gray-50 font-bold text-center leading-none w-full pt-14 pb-2 box-border md:text-3xl md:py-4 md:pl-6 md:text-left">
+            TODO
         </header>
     );
 };
