b422e44abe1857ba3fa55968eb60ba85400e8d3c
Sato Taisei <t.sei43rm@gmail.com>
2022-11-27 16:23:41 +0900

    ✨ add Loader

---
+++ src/components/Loader.tsx
@@ -0,0 +1,7 @@
+export const Loader = () => {
+    return (
+        <div className="flex justify-center">
+            <div className="animate-spin h-10 w-10 border-4 border-yellow-500 rounded-full border-t-transparent my-8"></div>
+        </div>
+    );
+};
