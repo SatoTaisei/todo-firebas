33fb14e553579b326799f83b2581b9195a1eb6b1
Sato Taisei <t.sei43rm@gmail.com>
2022-11-29 17:34:34 +0900

    ✨ add dark mode toggle button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,36 @@
+import Image from 'next/image';
+import { useState } from 'react';
+
 export const Header = () => {
+    // TODO: 初期値は各ユーザーの設定を読み込む
+    const [isDarkMode, setIsDarkMode] = useState(true);
+
     return (
-        <header className="absolute top-0 bg-neutral-900 border-b-2 border-neutral-800 text-2xl text-yellow-500 font-bold text-center leading-none w-full pt-8 pb-2 box-border md:text-3xl md:py-4 md:pl-6 md:text-left">
-            TODO
+        <header className="absolute flex items-center justify-between top-0 border-b-2 border-neutral-800 w-full py-5 pl-6 box-border">
+            <span className="text-2xl text-yellow-500 font-bold leading-none">
+                TODO
+            </span>
+            <button
+                type="button"
+                className="text-neutral-300 rounded-md hover:bg-neutral-700 p-2 mr-6"
+                onClick={() => setIsDarkMode(!isDarkMode)}
+            >
+                {isDarkMode ? (
+                    <Image
+                        src="/assets/images/DarkModeIcon.svg"
+                        alt=""
+                        width={20}
+                        height={20}
+                    />
+                ) : (
+                    <Image
+                        src="/assets/images/LightModeIcon.svg"
+                        alt=""
+                        width={20}
+                        height={20}
+                    />
+                )}
+            </button>
         </header>
     );
 };
