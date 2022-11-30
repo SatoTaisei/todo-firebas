fca9465bc718aedf149d72271ab244706b966c6d
Sato Taisei <t.sei43rm@gmail.com>
2022-11-30 20:27:25 +0900

    ⚡️ dark mode function

--- src/components/Button/ToggleModeButton.tsx
+++ src/components/Button/ToggleModeButton.tsx
@@ -1,31 +1,15 @@
 import Image from 'next/image';
-import { useCallback, useEffect, useState } from 'react';
+
+import { useDarkMode } from '@/hooks/useDarkMode';
 
 export const ToggleModeButton = () => {
-    const [isDarkMode, toggleTheme] = useState<boolean>(true);
-    const toggle = useCallback((isDark?: boolean) => {
-        if (typeof isDark === 'undefined') {
-            toggleTheme((state) => !state);
-            return;
-        }
-
-        toggleTheme(isDark);
-    }, []);
-
-    useEffect(() => {
-        if (isDarkMode) {
-            document.documentElement.classList.add('dark');
-        } else {
-            document.documentElement.classList.remove('dark');
-        }
-    }, [isDarkMode]);
+    const { isDarkMode, toggle } = useDarkMode();
 
     return (
         <button
             type="button"
             className="text-neutral-300 rounded-md hover:bg-neutral-100 dark:hover:bg-neutral-700 p-2 mr-6"
-            // onClick={() => setIsDarkMode(!isDarkMode)}
-            onClick={() => toggle()}
+            onClick={() => toggle(isDarkMode)}
         >
             {isDarkMode ? (
                 <Image
